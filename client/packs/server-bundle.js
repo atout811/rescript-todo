771c044d25e424ebaf1e16d78448a836e2f0ce3c
atout811 <dmcgfx811@gmail.com>
2022-08-17 02:21:41 +0200

    update server bundle

--- client/packs/server-bundle.js
+++ client/packs/server-bundle.js
@@ -1,8 +1,7 @@
 import ReactOnRails from "react-on-rails";
-
-import HelloWorld from "../bundles/HomePage/HomePage.bs";
+import { make as HomePage } from "../bundles/HomePage/HomePage.bs";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
+  HomePage,
 });
